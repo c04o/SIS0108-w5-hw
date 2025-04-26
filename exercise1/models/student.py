693a52826e406efc962a60f7cf856b8cd28ff8a1
c04o <c04o@proton.me>
2025-04-25 19:43:54 -0600

    Created Student class

---
+++ exercise1/models/student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, id, name, last_name, weight, height, sex, average):
+        self.id = id
+        self.name = name
+        self.last_name = last_name
+        self.weight = weight
+        self.height = height
+        self.sex = sex
+        self.average = average
